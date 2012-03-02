a036e419a5b0debd0ac00e2274e4e04c9fc26e78
sid <sid@sid-Rev-1-0.(none)>
2012-03-02 18:22:33 -0500

    added gui for gs client

--- overo-python/gs-client.py
+++ overo-python/gs-client.py
@@ -1,23 +1,83 @@
 import socket
 import sys
 import getch
+import Tkinter
 
-HOST, PORT = "192.168.23.2", 9999
-data = " ".join(sys.argv[1:])
-
-# SOCK_DGRAM is the socket type to use for UDP sockets
-sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-
-# As you can see, there is no connect() call; UDP has no connections.
-# Instead, data is directly sent to the recipient via sendto().
-
-x = 'a'
-byteSent = 0
-while x != 'q' :
-  #x = raw_input("command : ")
-  print "command : "
-  x = getch.getch()
-  sock.sendto(x + "\n", (HOST, PORT))
-  byteSent = byteSent + 1
+class gs_gui(Tkinter.Tk):
   
-print "Sent: {} Bytes".format(byteSent)
+  def __init__(self,parent):
+    Tkinter.Tk.__init__(self,parent)
+    #Track parent
+    #Gui's are a hierarchy of thimgs
+    #ex: Window-->Tab-->Pane-->Button
+    #    (parent) ---> children
+    #Good practice to mainatain a reference to your parent
+    self.parent = parent
+    
+    #Init the GUI
+    #create gui elements
+    self.inititalize()
+ 
+  def inititalize(self):
+    #instanstiate a layout manager
+    self.grid()
+    
+    #variable to store txt entered
+    self.entryVar = Tkinter.StringVar()
+    
+    #create a text entry widget
+    #we keep a refference to the txt box in order to
+    #read / manipulate it later
+    self.entry = Tkinter.Entry(self,textvariable=self.entryVar)
+    
+    #add to the layout manager
+    #sticky EW (East West) sticks it to the vertical edges 
+    #of the window
+    self.entry.grid(column=0,row=0,sticky='EW')
+    
+    #add event
+    self.entry.bind("<Return>",self.OnPressEnter)
+    
+    #set textvariable
+    self.entryVar.set("CMD")
+    
+    #create a button
+    #we do not keep a refference to the button since
+    #it will not be changed
+    button = Tkinter.Button(self,text="Send",command=self.OnButtonClick)
+    
+    #add it to the layout manager
+    button.grid(column=1,row=0)
+    
+    #add labels
+    self.labelVariable = Tkinter.StringVar()
+    label = Tkinter.Label(self, textvariable = self.labelVariable, anchor='w', fg="black", bg="white")
+    label.grid(column=0,row=1,columnspan=2,sticky='EW')
+    self.labelVariable.set("Ground Station")
+    
+    #Tell the layout manager to resize columns when the window
+    #is resized
+    self.grid_columnconfigure(0,weight=1)
+    self.resizable(True,False)
+
+    # Open Port
+    self.HOST, self.PORT = "192.168.23.3", 9999
+    # SOCK_DGRAM is the socket type to use for UDP sockets
+    self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    self.byteSent = 0
+    
+  def OnButtonClick(self):
+    self.labelVariable.set("sending : "+self.entryVar.get())
+    print self.entryVar.get()
+    self.sock.sendto(self.entryVar.get() + "\n", (self.HOST, self.PORT))
+    self.byteSent = self.byteSent + 1
+    print "Sent: {} Msgs".format(self.byteSent)
+    
+  def OnPressEnter(self,event):
+    self.OnButtonClick()
+    
+    
+if __name__ == "__main__":
+    app = gs_gui(None)
+    app.title('zepplin gs')
+    app.mainloop()
